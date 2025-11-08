9e53d0a155a1828ae1c85f513c68ab8dbb7d74ec
rakizakiedu <rise.akizaki@maine.edu>
2025-11-08 11:53:09 -0500

    Updated program for cleaning course data
    Also updated cleaned course data to better fit the fields in the schema

--- Project phase 2/data/data_cleaners/CourseCleaner.py
+++ Project phase 2/data/data_cleaners/CourseCleaner.py
@@ -6,20 +6,26 @@ import pandas as pd
 # absolute filepath.
 file_path = r"C:\Users\Work\Documents\GitHub\Study_Buddy_hub\Project phase 2\data\usm_courses.csv"
 df = pd.read_csv(file_path)
+cdf = pd.DataFrame(columns=['course_id', 'course_code', 'course_name', 'college_id'])
 
 file_height = len(df)
 
 # Separates course abbreviation + catalog # from the course name, and saves it as the course code
 def CleanData():
     for i in range (file_height):
+        # name_length = len(df.loc[i, 'course_name'])
+        # course_code = df.loc[i,'course_name'][:3] + df.loc[i,'course_name'][4:7] # course code is course abr + catalog #. Removes whitespace
+        # df.loc[i,'course_code'] = course_code # adds saved course code to the code column
+        # df.loc[i,'course_name'] = df.loc[i,'course_name'][10:name_length] # removes course code from course name
+
         name_length = len(df.loc[i, 'course_name'])
         course_code = df.loc[i,'course_name'][:3] + df.loc[i,'course_name'][4:7] # course code is course abr + catalog #. Removes whitespace
-        df.loc[i,'course_code'] = course_code # adds saved course code to the code column
-        df.loc[i,'course_name'] = df.loc[i,'course_name'][10:name_length] # removes course code from course name
+        course_name = df.loc[i,'course_name'][10:name_length]
+        cdf.loc[i] = [i, course_code, course_name, None]
     print("Finished cleaning course data")
 
 CleanData()
-
 output_path = "usm_courses_clean.csv"
-df.to_csv(output_path, index = False) # index = False prevents indices from being the first column
+cdf.to_csv(output_path, index = False) # index = False prevents indices from being the first column
+
 print("Copy of cleaned courses saved to " + output_path)
