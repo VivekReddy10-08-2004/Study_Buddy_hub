0d15b361f5a3df083b6e983144136e5b65034755
rakizakiedu <rise.akizaki@maine.edu>
2025-11-13 09:25:01 -0500

    Added another query for analysis
    Wasn't sure if I needed another query for the analysis, but did one just in case.

--- Project phase 2/sql/User_Management/Other SQL (Data Insertion, Relationship Testing, etc)/User_Management_Optimization_Query.sql
+++ Project phase 2/sql/User_Management/Other SQL (Data Insertion, Relationship Testing, etc)/User_Management_Optimization_Query.sql
@@ -1,6 +1,6 @@
 use StudyBuddy;
 
--- Query Example for Analysis
+-- Query Example 1 for Analysis
 -- Find course (names) which are part of USM, and are ART courses
 
 -- Query 1
@@ -12,8 +12,8 @@ WHERE cl.college_name LIKE '%University of Southern Maine%'
 AND cr.course_code LIKE '%ART%';
 
 -- Query 2
-CREATE INDEX idx_college_name ON Colleges (college_name);
-CREATE INDEX idx_course_code ON Courses (course_code);
+CREATE INDEX college_name_index ON Colleges (college_name);
+CREATE INDEX course_code_index ON Courses (course_code);
 
 EXPLAIN ANALYZE
 SELECT cr.course_name, cl.college_name
@@ -21,3 +21,27 @@ FROM Courses cr
 JOIN Colleges cl ON cr.college_id = cl.college_id
 WHERE cl.college_name LIKE 'University of Southern Maine%'   
 AND cr.course_code LIKE 'ART%';               
+
+
+-- Query Example 2 for Analysis
+-- Find course (names) which are part of USM, and are COS courses
+
+SET optimizer_switch='derived_merge=off'; -- To prevent optimization during testing
+
+-- Query 1
+EXPLAIN ANALYZE
+SELECT course_name 
+FROM (
+	SELECT *
+	FROM Colleges
+	JOIN Courses USING (college_id)
+)
+AS joinedTables
+WHERE joinedTables.course_code LIKE 'COS%';
+
+-- Query 2
+EXPLAIN ANALYZE
+SELECT cr.course_name
+FROM Courses cr
+JOIN Colleges cl ON cr.college_id = cl.college_id
+WHERE cr.course_code LIKE 'COS%';
