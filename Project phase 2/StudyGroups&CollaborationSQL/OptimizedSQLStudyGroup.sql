a50b814fe82f3cd856bc1bf57943285351c4d197
JakeCraig22 <159073970+JakeCraig22@users.noreply.github.com>
2025-11-14 19:19:24 -0500

    Combining triggers from previous SQL File into this code
    Added a trigger to enforce single owner role in Group_Member and a constraint to ensure end time is after start time in Study_Session.

--- Project phase 2/StudyGroups&CollaborationSQL/OptimizedSQLStudyGroup.sql
+++ Project phase 2/StudyGroups&CollaborationSQL/OptimizedSQLStudyGroup.sql
@@ -249,3 +249,31 @@ SELECT resource_id, title, filetype, source
 FROM Resource
 ORDER BY resource_id DESC
 LIMIT 25;
+
+-- Trigger to enforce one owner only
+-- Will work on optimizing this later... 
+DELIMITER //
+CREATE TRIGGER one_owner_only
+BEFORE INSERT ON Group_Member
+FOR EACH ROW
+BEGIN
+    DECLARE owner_count INT DEFAULT 0;
+    -- count
+
+    -- only check when trying to add an owner
+    IF NEW.role = 'owner' THEN
+        SELECT COUNT(*) INTO owner_count
+        FROM Group_Member
+        WHERE group_id = NEW.group_id
+        AND role = 'owner';
+        IF owner_count > 0 THEN
+            -- stop the insert by setting a bad value or doing nothing
+            SET NEW.role = NULL;
+        END IF;
+    END IF;
+END//
+DELIMITER ;
+
+-- making sure end time before start time
+ALTER TABLE Study_Session
+ADD CONSTRAINT chk_time CHECK (end_time > start_time);
