7e39d7a541fd1955c1ff2d01f51f1ccbbd73d5b5
Vivek_Reddy <144725047+VivekReddy10-08-2004@users.noreply.github.com>
2025-11-14 22:41:54 -0500

    Adding a semicolon.
    Small syntax error, was missing a semicolon at the end of the join Select statements

--- Project phase 2/sql/User_Management/Other SQL (Data Insertion, Relationship Testing, etc)/User_Management_Relationship_Testing.sql
+++ Project phase 2/sql/User_Management/Other SQL (Data Insertion, Relationship Testing, etc)/User_Management_Relationship_Testing.sql
@@ -10,8 +10,8 @@ VALUES ('bobjones@gmail.com', 12345, 'bob', 'jones', 'Senior', 813, 1);
 
 SELECT m.major_name, u.first_name
 FROM Users u
-JOIN Majors m ON m.major_id = u.major_id 
+JOIN Majors m ON m.major_id = u.major_id; 
 
 SELECT cl.college_name, u.first_name
 FROM Users u
-JOIN Colleges cl ON cl.college_id = u.college_id 
+JOIN Colleges cl ON cl.college_id = u.college_id; 
