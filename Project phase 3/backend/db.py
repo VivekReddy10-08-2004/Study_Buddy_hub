144b497c7a7bb696e6a9848c90f46a169b5c542f
JakeCraig22 <159073970+JakeCraig22@users.noreply.github.com>
2025-11-29 15:32:57 -0500

    Add database connection function using mysql.connector

--- Project phase 3/backend/db.py
+++ Project phase 3/backend/db.py
@@ -1,1 +1,13 @@
+import mysql.connector
+import os
 
+def get_db_connection():
+    return mysql.connector.connect(
+        host=os.getenv("MYSQL_HOST", "localhost"),
+        port=int(os.getenv("MYSQL_PORT", 3306)),
+        user=os.getenv("MYSQL_USER", "root"),
+        password=os.getenv("MYSQL_PASSWORD", ""),
+        database=os.getenv("MYSQL_DB", "StudyBuddy")
+    )
+
+#feel free to change this -- putting this here for my routes as of right now. Also feel free to delete this comment!
